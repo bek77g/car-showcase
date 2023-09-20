9e6a7849bb85992f9094fd7e19314fe95043f7e2
bek77g <d.zheenkul@gmail.com>
2023-09-20 23:09:40 +0600

    add: show more btn

--- components/index.ts
+++ components/index.ts
@@ -7,6 +7,7 @@ import CustomFilter from '@/components/CustomFilter';
 import SearchManufacturer from '@/components/SearchManufacturer';
 import CarCard from '@/components/CarCard';
 import CarDetails from '@/components/CarDetails';
+import ShowMore from '@/components/ShowMore';
 
 export {
   Hero,
@@ -18,4 +19,5 @@ export {
   SearchManufacturer,
   CarCard,
   CarDetails,
+  ShowMore,
 };
