a1bd627fe4ebc767141a6890c6dd583b1095da4b
bek77g <d.zheenkul@gmail.com>
2023-09-20 16:30:54 +0600

    init: CarCardDetail component

---
+++ components/CarDetails.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps;
+}
+
+const CarDetails = () => {
+  return <div>CarDetails</div>;
+};
+
+export default CarDetails;
