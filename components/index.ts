3779517c6603ef3d0e95a40269d9f3a51a54499a
bek77g <d.zheenkul@gmail.com>
2023-09-20 14:18:24 +0600

    init: repo to github

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+import Hero from './Hero';
+import CustomButton from '@/components/CustomButton';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+export { Hero, CustomButton, Navbar, Footer };
